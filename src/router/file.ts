77bb49ab2d3cd353c4f3dc203b0be0c2ab20318b
RupakBoral <boralrupak@gmail.com>
2026-04-03 00:35:26 +0530

    add: extracting, chunking, embeding

--- src/router/file.ts
+++ src/router/file.ts
@@ -1,10 +1,29 @@
 import { Request, Response, Router } from "express";
 import multer from "multer";
+import { extractPDF, ExtractPDFType } from "../services/pdf";
+import { chunkPDF } from "../services/chunking";
+import { EmbedChunk } from "../services/embedding";
 
 const upload = multer({ storage: multer.memoryStorage() })
 export const fileRouter = Router();
 
-fileRouter.post('/upload', upload.single("file"), (req: Request, res: Response) => {
-    console.log("File: ", req.file?.buffer);
-    res.status(200).json({ success: true, message: "Success" });
+fileRouter.post('/ingest', upload.single("file"), async (req: Request, res: Response) => {
+    try {
+        const pdfFile = req.file?.buffer;
+
+        if (!pdfFile) {
+            res.status(422).json({ success: false, message: "No PDF content was provided." });
+            return;
+        }
+
+        const extractedResult: ExtractPDFType = await extractPDF(pdfFile);
+        const chunks: string[] = chunkPDF(extractedResult);
+        const embedObject = new EmbedChunk();
+        const embeddings = await embedObject.embeddingChunks(chunks);
+
+        res.status(200).json({ success: true, message: "Success" });
+    } catch (error) {
+        console.error("Error..", error);
+        res.status(500).json({ success: false, message: "Failed, Please try again," });
+    }
 });
---
+++ src/services/embedding.ts
@@ -0,0 +1,29 @@
+import ollama, { EmbeddingsRequest } from 'ollama';
+
+export class EmbedChunk {
+    private embededChunks: number[][];
+
+    constructor() {
+        this.embededChunks = [];
+    }
+
+    private embeddingSingleChunk = async (chunk: string) => {
+        console.info("Hello");
+        const request: EmbeddingsRequest = {
+            model: 'nomic-embed-text',
+            prompt: chunk,
+        };
+        const response = await ollama.embeddings(request);
+        this.embededChunks.push(response.embedding);
+    }
+
+    public embeddingChunks = async (chunks: string[]): Promise<number[][]> => {
+        try {
+            await Promise.all(chunks.map(chunk => this.embeddingSingleChunk(chunk)));
+            return this.embededChunks;
+
+        } catch (error) {
+            throw new Error(`Error while emdeding chunks. ${error}`);
+        }
+    }
+}
